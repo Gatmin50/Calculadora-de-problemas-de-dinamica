76dc53e243e076324b4e474654bd8c133e8e7ab9
Gatmin50 <tonilube6@gmail.com>
2024-04-19 21:32:02 +0200

    Update

--- Codigo/Calculadora_de_dinámica-OS.py
+++ Codigo/Calculadora_de_dinámica-OS.py
@@ -1,6 +1,10 @@
 import customtkinter as ctk
 import tkinter as tk
 from tkinter import ttk
+import os
+import platform
+import requests
+import shutil
 import Intro
 import Calculadoras.Una_masa as Una_masa
 import Calculadoras.Dos_masas as Dos_masas
@@ -11,15 +15,64 @@ import Calculadoras.Trabajo_inclinado as Trabajo_inclinado
 import Calculadoras.Trabajo_circular as Trabajo_circular
 import Calculadoras.Ondas_armónicas as Ondas_armónicas
 import Calculadoras.Nuclear as Nuclear
+import Calculadoras.Interacción_electrmagnética as Interaccion_Electromagnetica
 
-Intro.animacion_inicial()
+# Using os.name
+os_name = os.name
+if os_name == 'nt':
+    print("Windows OS detected")
+    # URL del archivo de imagen en GitHub
+    url = 'https://raw.githubusercontent.com/Gatmin50/Calculadora-de-problemas-de-dinamica/main/Icono.ico'
+
+    # Directorio donde deseas guardar el archivo descargado
+    directorio = 'C:/calculadora_de_dinamica/'
+    ruta_local = os.path.join(directorio, 'Icono.ico')
+
+    # Verificar si el directorio existe y, si no, crearlo
+    if not os.path.exists(directorio):
+        os.makedirs(directorio)
+
+    # Verificar si el archivo ya existe antes de descargarlo
+    if not os.path.exists(ruta_local):
+        try:
+            # Descargar la imagen desde GitHub
+            response = requests.get(url, stream=True)
+            response.raise_for_status()
+        
+            # Guardar la imagen en la ruta local
+            with open(ruta_local, 'wb') as file:
+                shutil.copyfileobj(response.raw, file)
+        except Exception as e:
+            print(f"Error al descargar el archivo: {e}")
+        else:
+            print(f"El archivo ya existe en {ruta_local}")
+elif os_name == 'posix':
+    print("Linux or macOS OS detected")
+else:
+    print(f"Unknown OS: {os_name}")
+
+# Using platform module (optional)
+system = platform.system()
+release = platform.release()
+version = platform.version()
+
+print(f"System: {system}")
+print(f"Release: {release}")
+print(f"Version: {version}")
+
+#Intro.animacion_inicial()
 
 # Crear la ventana del menu de la calculadora
 ventana1 = ctk.CTk()
 ventana1.title("Menu calculadora de Dinámica")
-ventana1.geometry('710x300+400+200')
 ventana1.configure(fg_color="black")
 ctk.set_default_color_theme("dark-blue")
+# Obtener el tamaño de la pantalla
+screen_width = ventana1.winfo_screenwidth()
+screen_height = ventana1.winfo_screenheight()
+# Establecer el tamaño de la ventana para que ocupe toda la pantalla
+ventana1.geometry(f"{screen_width}x{screen_height}")
+ventana1.iconbitmap(ruta_local)
 
 def programa_plano_inclinado():
     Una_masa.programa_plano_inclinado()
@@ -47,6 +100,9 @@ def Ondas():
 
 def nuclear():
     Nuclear.calculo_nuclear()
+
+def Electromagnetismo():
+    Interaccion_Electromagnetica.electromagnetismo()
 
 # despliege de secciones
 def seleccionar_opcion(event):
@@ -81,6 +137,8 @@ def cerrar_ventana4():
     boton_calculadora_te.grid(column=3, row=3, columnspan=2, padx=10, pady=10)
     btn_cerrar.grid(column=2, row=5, columnspan=3, padx=10, pady=10)
     boton_calculadora_ondas_armonicas.grid(column=2, row=3, padx=10, pady=10)
+    boton_calculadora_nuclear.grid(column=2, row=3, columnspan=2, padx=10, pady=10)
+    boton_calculadora_elctromagnética.grid(column=4, row=3, columnspan=2, padx=10, pady=10)
     ventana1.geometry('710x300+400+200')
 
 btn_back = ctk.CTkButton(ventana1, text="Atras", font=("Helvetica", 10, "bold"), command=cerrar_ventana4)
@@ -92,7 +150,9 @@ def traspaso1():
     boton_calculadora.grid_remove()
     boton_calculadora_te.grid_remove()
     btn_cerrar.grid_remove()
-    boton_calculadora_ondas_armonicas.grid_remove
+    boton_calculadora_ondas_armonicas.grid_remove()
+    boton_calculadora_nuclear.grid_remove()
+    boton_calculadora_elctromagnética.grid_remove()
     ventana1.title("Menu calculadora de Trabajo y Energía")
     combobox.grid(row=0, column=1, padx=10, pady=10)
     etiqueta_resultado.grid(row=1, column=1, padx=10, pady=10)
@@ -101,18 +161,20 @@ def traspaso1():
     ventana1.geometry('390x150')
 
 # Crear los botones de las calculadoras
-boton_calculadora_plano_inclinado = ctk.CTkButton(ventana1, text="Calculadora de plano inclinado", command=programa_plano_inclinado , font=("Helvetica", 15, "bold"),corner_radius=20)
-boton_calculadora_plano_inclinado.grid(column=2, row=2, padx=10, pady=10)
+boton_calculadora_plano_inclinado = ctk.CTkButton(ventana1, text="Calculadora de plano inclinado", command=programa_plano_inclinado , font=("Helvetica", 15, "bold"), corner_radius=20)
+boton_calculadora_plano_inclinado.grid(column=0, row=0, padx=10, pady=10)
 boton_calculadora_plano_inclinado_con_dos_cuerpos = ctk.CTkButton(ventana1, text="Calculadora de plano inclinado \n con dos cuerpos unidos", command=programa_plano_inclinado_con_dos_cuerpos , font=("Helvetica", 15, "bold"),corner_radius=20)
-boton_calculadora_plano_inclinado_con_dos_cuerpos.grid(column=4, row=2, padx=10, pady=10)
+boton_calculadora_plano_inclinado_con_dos_cuerpos.grid(column=3, row=0, padx=10, pady=10)
 boton_calculadora = ctk.CTkButton(ventana1, text="Calculadora", command=mini_calculadora , font=("Helvetica", 15, "bold"),corner_radius=20)
-boton_calculadora.grid(column=2, row=4, columnspan=2, padx=10, pady=10)
+boton_calculadora.grid(column=0, row=1, padx=10, pady=10)
 boton_calculadora_te = ctk.CTkButton(ventana1, text="Problemas de trabajo y energia", command=traspaso1 , font=("Helvetica", 15, "bold"),corner_radius=20)
-boton_calculadora_te.grid(column=3, row=3, columnspan=2, padx=10, pady=10)
+boton_calculadora_te.grid(column=2, row=1, padx=10, pady=10)
 boton_calculadora_ondas_armonicas = ctk.CTkButton(ventana1, text="Calculadora de \n ondas armonicas", command=Ondas, font=("Helvetica", 15, "bold"),corner_radius=20)
-boton_calculadora_ondas_armonicas.grid(column=2, row=3, padx=10, pady=10)
+boton_calculadora_ondas_armonicas.grid(column=0, row=3, padx=10, pady=10)
 boton_calculadora_nuclear = ctk.CTkButton(ventana1, text="Calculadora nuclear", command=nuclear, font=("Helvetica", 15, "bold"),corner_radius=20)
-boton_calculadora_nuclear.grid(column=3, row=4, columnspan=2, padx=10, pady=10)
+boton_calculadora_nuclear.grid(column=2, row=3, padx=10, pady=10)
+boton_calculadora_elctromagnética = ctk.CTkButton(ventana1, text="Calculadora Electromagnética", command=Electromagnetismo, font=("Helvetica", 15, "bold"),corner_radius=20)
+boton_calculadora_elctromagnética.grid(column=4, row=3, padx=10, pady=10)
 
 def cerrar_ventana():
     ventana1.destroy()
